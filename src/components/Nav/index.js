d281508b8fa50a3dc4992840fce287031ad84e44
vhivestate <garcia.valeria001@gmail.com>
2022-08-01 01:45:55 -0600

    nav styling

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -22,11 +22,6 @@ function Nav(props) {
         </h2>
         <nav>
             <ul className='flex-row'>
-                <li className='mx-2'>
-                    <a href='#about'>
-                        About me
-                    </a>
-                </li>
                 {categories.map((category) => (
             <li
               className={`mx-1 ${
