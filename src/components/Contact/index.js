eb313910ca9b6dd0a2c808249393f21c0fa28444
vhivestate <garcia.valeria001@gmail.com>
2022-08-01 15:49:27 -0600

    added contact

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,7 +1,70 @@
 import React, { useState } from 'react';
+import { validateEmail } from '../../utils/helper';
 
 function ContactForm() {
+    const [formState, setFormState] = useState({ name: '', email: '', message: '' });
+    const { name, email, message } = formState;
+    const [errorMessage, setErrorMessage] = useState('');
 
-}
+    function handleChange(e) {
+        if (e.target.name === 'email') {
+            const isValid = validateEmail(e.target.value);
+            console.log(isValid);
+            // isValid conditional statement
+            if (!isValid) {
+                setErrorMessage('Your email is invalid.');
+            } else {
+                if (!e.target.value.length) {
+                  setErrorMessage(`${e.target.name} is required.`);
+                } else {
+                  setErrorMessage('');
+                }
+                //only updates if form passed quality
+                if (!errorMessage) {
+                    setFormState({ ...formState, [e.target.name]: e.target.value });
+                  }
+              }
+              console.log('errorMessage', errorMessage);
+          } 
+           
+         //get input values
+        setFormState({...formState, [e.target.name]: e.target.value })
+        // console.log(formState);
+      }
 
-export default ContactForm;
+      //get input value when form is submitted
+      function handleSubmit(e) {
+        e.preventDefault();
+        console.log(formState);
+      }
+
+    return (
+        <section>
+          <h1>Contact me</h1>
+          <form id="contact-form" onSubmit={handleSubmit}>
+            <div>
+              <label htmlFor="name">Name:</label>
+              <input type="text" name="name" defaultValue={name} onBlur={handleChange} />
+            </div>
+            <div>
+              <label htmlFor="email">Email address:</label>
+              <input type="email" name="email" defaultValue={email} onBlur={handleChange}/>
+            </div>
+            <div>
+              <label htmlFor="message">Message:</label>
+              <textarea name="message" rows="5" defaultValue={message} onBlur={handleChange}/>
+            </div>
+            {errorMessage && (
+                <div>
+                    <p className="error-text">{errorMessage}</p>
+                </div>
+                )}
+            <button type="submit">Submit</button>
+          </form>
+        </section>
+        );
+
+    // JSX
+    }
+    
+    export default ContactForm;
--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -4,7 +4,7 @@ function Footer() {
 
   return (
     <section className="my-5" >
-      <h1 id="Footer"> Github, email, mail</h1>
+      <h1 id="Footer"> footer</h1>
       <div className="my-2">
         <p>
       </p>
