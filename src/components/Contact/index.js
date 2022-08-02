e3e4c76551ff46544311f4498c40135a6536085f
vhivestate <garcia.valeria001@gmail.com>
2022-08-02 12:25:50 -0600

    added resume files

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -41,7 +41,7 @@ function ContactForm() {
     return (
         <section>
           <h1>Contact me</h1>
-          <form id="contact-form" onSubmit={handleSubmit}>
+          <form action="" id="contact-form" onSubmit={handleSubmit}>
             <div>
               <label htmlFor="name">Name:</label>
               <input type="text" name="name" defaultValue={name} onBlur={handleChange} />
--- src/components/Portfolio/index.js
+++ src/components/Portfolio/index.js
@@ -4,6 +4,8 @@ import Schedule from '../../assets/photo/work-schedule.png'
 import Weather from '../../assets/photo/weather.png'
 import Appt from '../../assets/photo/appt-booker.png'
 import Team from '../../assets/photo/team.png'
+import '../../index.css'
+
 
 
 function Portfolio() {
@@ -11,7 +13,7 @@ function Portfolio() {
   return (
     <section>
       <h1>My Projects:</h1>
-      <img src={Quiz} alt=''></img>
+      <img src={Quiz} alt='https://github.com/vhivestate/Code-Quiz' href='https://github.com/vhivestate/Code-Quiz'></img>
       <img src={Schedule} alt=''></img>
       <img src={Weather} alt=''></img>
       <img src={Appt} alt=''></img>
