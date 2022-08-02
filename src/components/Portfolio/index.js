d9ab5ad086022536c6669731ef99b53c7731f8cd
vhivestate <garcia.valeria001@gmail.com>
2022-08-01 19:38:58 -0600

    added projects

--- src/components/Portfolio/index.js
+++ src/components/Portfolio/index.js
@@ -1,12 +1,23 @@
 import React from 'react';
-import photo from '../../assets/photo/codeQuiz.png'
+import Quiz from '../../assets/photo/codeQuiz.png'
+import Schedule from '../../assets/photo/work-schedule.png'
+import Weather from '../../assets/photo/weather.png'
+import Appt from '../../assets/photo/appt-booker.png'
+import Team from '../../assets/photo/team.png'
+
 
 function Portfolio() {
 
   return (
     <section>
       <h1>My Projects:</h1>
-      <img src={photo} alt=''></img>
+      <img src={Quiz} alt=''></img>
+      <img src={Schedule} alt=''></img>
+      <img src={Weather} alt=''></img>
+      <img src={Appt} alt=''></img>
+      <img src={Team} alt=''></img>
+
+
     </section>
   );
 }
