273bb0c958324a1f51eac3997efd79fe19247fd8
vhivestate <garcia.valeria001@gmail.com>
2022-08-01 18:39:18 -0600

    added photo

--- src/components/Portfolio/index.js
+++ src/components/Portfolio/index.js
@@ -1,11 +1,12 @@
 import React from 'react';
+import photo from '../../assets/photo/codeQuiz.png'
 
-function Portfolio(props) {
-  const { currentCategory } = props;
+function Portfolio() {
 
   return (
     <section>
       <h1>My Projects:</h1>
+      <img src={photo}></img>
     </section>
   );
 }
