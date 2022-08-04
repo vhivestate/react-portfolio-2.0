8da905c95b91f2db00d6b3f04236b931628761b0
vhivestate <garcia.valeria001@gmail.com>
2022-08-04 11:46:44 -0600

    refactor

--- src/apptest.js
+++ src/apptest.js
@@ -1,8 +1,11 @@
-import React, {useState} from 'react';
-import About from "./components/About";
-import Contact from "./components/Contact";
-import Portfolio from './components/Portfolio';
-import Nav from "./components/Nav"
+import React, { useState } from 'react';
+import Nav from './components/Nav';
+import About from './components/About';
+import Contact from './components/Contact';
+import Footer from './components/Footer';
+import PortfolioCom from './components/Portfolio';
+import Resume from './components/Resume';
+import Header from './components/Header'
 
 function App() {
     const [currentPage, setCurrentPage] = useState(0)
@@ -12,8 +15,10 @@ function App() {
             return<About></About>;
         }else if(currentPage === 1){
             return <Contact></Contact>
-        } else {
+        } else if (currentPage ===2 ){
             return <Portfolio></Portfolio>
+        } else {
+            return <Resume></Resume>
         }
     }
     return (
--- src/navtest.js
+++ src/navtest.js
@@ -1,35 +1,14 @@
-import React, {useState, useEffect} from 'react';
-import About from "./components/About";
-import Contact from "./components/Contact";
-import Portfolio from './components/Portfolio';
+import React from 'react'
 
 function Nav(props) {
+  return (
+    <div>
+        <button onClick={() => props.setCurrentPage(0)}>About</button>
+        <button onClick={() => props.setCurrentPage(1)}>Contact</button>
+        <button onClick={() => props.setCurrentPage(2)}>Portfolio</button>
+        <button onClick={() => props.setCurrentPage(3)}>Resume</button>
+    </div>
+  )
+}
 
-    // <About/> === [0];
-    
-    // const [currentPage, setCurrentPage ] = useState(0);
-    // var pageState = useState(0);
-    // var page = pageState.About[0];
-    // var setPage = pageState.Contact[1];
-    
-    // function handleContactClick(){
-    //     setCurrentPage({Contact});
-    //     // setPage('contact');
-    // }
-    
-
-
-    return (
-        <div>
-            <button onClick={() => setCurrentPage === props.currentPage[0]}>
-                About
-            </button>
-            <button onClick={() => setCurrentPage === (props.Contact.currentPage[1])}>
-                Contact
-            </button>
-            <button onClick={() => setCurrentPage === (props.Portfolio.currentPage[2])}>
-                Portfolio
-            </button>
-        </div>
-    )
-  }
+export default Nav
